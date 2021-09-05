9275c76c949872accbda3e0795d31b3a661b85a1
Abdul Salam <abdulsalamsabil@gmail.com>
2021-09-05 17:43:00 +0700

    open music v2

---
+++ migrations/1630797074192_create-table-playlists.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+
+  pgm.sql("INSERT INTO users(id, username, password, fullname) VALUES ('old_playlists', 'old_playlists', 'old_playlists', 'old playlists')");
+  pgm.sql("UPDATE playlists SET owner = 'old_playlists' WHERE owner = NULL");
+  pgm.addConstraint('playlists', 'fk_playlits.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
